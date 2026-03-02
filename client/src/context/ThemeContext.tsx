91a44768c998bbc528893bf22bf382b9a0f3285f
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2026-03-02 18:08:45 +0500

    added sidebar functionality

--- client/src/context/ThemeContext.tsx
+++ client/src/context/ThemeContext.tsx
@@ -1,7 +1,7 @@
 import {createContext, useContext, useEffect, useState} from 'react'
 
 interface ThemeContextType {
-    theme: String;
+    theme: string;
     toggleTheme: () => void
 }
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
