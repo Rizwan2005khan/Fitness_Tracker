134913259e18fe6ab729894f8e7d90039f56c100
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2026-03-07 22:51:42 +0500

    added strapi for backend development

---
+++ server/src/api/activity-l-log/routes/activity-l-log.ts
@@ -0,0 +1,7 @@
+/**
+ * activity-l-log router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::activity-l-log.activity-l-log');
---
+++ server/src/api/activity-l-log/services/activity-l-log.ts
@@ -0,0 +1,7 @@
+/**
+ * activity-l-log service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::activity-l-log.activity-l-log');
